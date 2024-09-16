4f0cd692f58e0be1c021037b2817e6aa2bf06442
Aleksei Viunik <a79643739989@yandex.ru>
2024-09-16 18:44:28 +0200

    Add logging to ProductsView.php

--- src/View/ProductsView.php
+++ src/View/ProductsView.php
@@ -4,16 +4,27 @@ namespace Raketa\BackendTestTask\View;
 
 use Raketa\BackendTestTask\Repository\Entity\Product;
 use Raketa\BackendTestTask\Repository\ProductRepository;
+use Psr\Log\LoggerInterface;
 
 readonly class ProductsView
 {
     public function __construct(
-        private ProductRepository $productRepository
+        private ProductRepository $productRepository,
+        private LoggerInterface $logger
     ) {
     }
 
     public function toArray(string $category): array
     {
+        try {
+            $products = $this->productRepository->getByCategory($category);
+        } catch (\Exception $e) {
+            $this->logger->error('Failed to retrieve products by category', [
+                'error' => $e->getMessage(),
+                'category' => $category
+            ]);
+            return [];
+        }
         return array_map(
             fn (Product $product) => [
                 'id' => $product->getId(),
@@ -23,7 +34,7 @@ readonly class ProductsView
                 'thumbnail' => $product->getThumbnail() ?? '',
                 'price' => $product->getPrice(),
             ],
-            $this->productRepository->getByCategory($category)
+            $products
         );
     }
 }
