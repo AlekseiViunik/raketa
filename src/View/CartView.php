1f8922dafd871c790deab53524d988c579db3cc9
Aleksei Viunik <a79643739989@yandex.ru>
2024-09-16 18:47:41 +0200

    Add logging to CartView.php

--- src/View/CartView.php
+++ src/View/CartView.php
@@ -6,11 +6,13 @@ namespace Raketa\BackendTestTask\View;
 
 use Raketa\BackendTestTask\Domain\Cart;
 use Raketa\BackendTestTask\Repository\ProductRepository;
+use Psr\Log\LoggerInterface;
 
 readonly class CartView
 {
     public function __construct(
-        private ProductRepository $productRepository
+        private ProductRepository $productRepository,
+        private LoggerInterface $logger
     ) {
     }
 
@@ -33,40 +35,50 @@ readonly class CartView
         $total = 0;
         $data['items'] = [];
 
-        $productUuids = array_map(fn($item) => $item->getProductUuid(), $cart->getItems());
-        $products = $this->productRepository->getByUuids($productUuids);
-        $productsMap = [];
+        try {
+            $productUuids = array_map(fn($item) => $item->getProductUuid(), $cart->getItems());
+            $products = $this->productRepository->getByUuids($productUuids);
 
-        foreach ($products as $product) {
-            $productsMap[$product->getUuid()] = $product;
-        }
-
-        foreach ($cart->getItems() as $item) {
-            $product = $productsMap[$item->getProductUuid()] ?? null;
-
-            if (!$product) {
-                continue;
+            if (empty($products)) {
+                $this->logger->warning("No products found for the provided UUIDs: " . implode(', ', $productUuids));
             }
 
-            $itemTotal = $item->getPrice() * $item->getQuantity();
-            $total += $itemTotal;
+            $productsMap = [];
+            foreach ($products as $product) {
+                $productsMap[$product->getUuid()] = $product;
+            }
 
-            $data['items'][] = [
-                'uuid' => $item->getUuid(),
-                'price' => $item->getPrice(),
-                'total' => $itemTotal,
-                'quantity' => $item->getQuantity(),
-                'product' => [
-                    'id' => $product->getId(),
-                    'uuid' => $product->getUuid(),
-                    'name' => $product->getName(),
-                    'thumbnail' => $product->getThumbnail(),
-                    'price' => $product->getPrice(),
-                ],
-            ];
+            foreach ($cart->getItems() as $item) {
+                $product = $productsMap[$item->getProductUuid()] ?? null;
+
+                if (!$product) {
+                    $this->logger->warning("Product not found for UUID: " . $item->getProductUuid());
+                    continue;  // Пропускаем товар, если его нет в списке
+                }
+
+                $itemTotal = $item->getPrice() * $item->getQuantity();
+                $total += $itemTotal;
+
+                $data['items'][] = [
+                    'uuid' => $item->getUuid(),
+                    'price' => $item->getPrice(),
+                    'total' => $itemTotal,
+                    'quantity' => $item->getQuantity(),
+                    'product' => [
+                        'id' => $product->getId(),
+                        'uuid' => $product->getUuid(),
+                        'name' => $product->getName(),
+                        'thumbnail' => $product->getThumbnail(),
+                        'price' => $product->getPrice(),
+                    ],
+                ];
+            }
+
+            $data['total'] = $total;
+
+        } catch (\Exception $e) {
+            $this->logger->error('Failed to fetch products for cart: ' . $cart->getUuid(), ['exception' => $e]);
         }
-
-        $data['total'] = $total;
 
         return $data;
     }
